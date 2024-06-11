08e6e20fc5040e5f5f7a676cd90ba0c11f30bd41
Pierpaolo Mariani <pierpaolo.mariani@vaimoo.com>
2024-06-11 10:41:42 +0200

    setup TableView
    add a tableView to our MoviesListViewController for displaying movies

--- Movies/View/MoviesListViewController.swift
+++ Movies/View/MoviesListViewController.swift
@@ -8,9 +8,52 @@
 import UIKit
 
 class MoviesListViewController: UIViewController {
-
+    
+    //MARK: view objects
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupTableView()
+        setupDelegate()
     }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        setupConstraints()
+    }
+    private func setupDelegate() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setupConstraints() {
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
+extension MoviesListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? UITableViewCell  else { return UITableViewCell() }
+        var content = cell.defaultContentConfiguration()
+        content.text = "Row"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
 }
 
